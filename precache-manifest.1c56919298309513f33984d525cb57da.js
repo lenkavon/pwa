0ea59d16e0876506af8b2987c9e85ff22bd1f3b8
lenkavon <lenka.vondr@gmail.com>
2019-10-03 13:20:18 +0200

    Updates

---
+++ precache-manifest.1c56919298309513f33984d525cb57da.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f3b3b3ee291b3ca3d739e6fabdc115e",
+    "url": "/pwa/static/media/newVersionAvailablePopup.3f3b3b3e.jpg"
+  },
+  {
+    "revision": "3765cd0fddc2d9c5d9a3",
+    "url": "/pwa/static/css/main.a95151e3.chunk.css"
+  },
+  {
+    "revision": "4853e24fdebc62533ac1fe20913b3f66",
+    "url": "/pwa/static/media/network-only.4853e24f.png"
+  },
+  {
+    "revision": "0598b3e3bc8680ca6a2c",
+    "url": "/pwa/static/js/1.0598b3e3.chunk.js"
+  },
+  {
+    "revision": "da52b829206605462073",
+    "url": "/pwa/static/js/runtime~main.da52b829.js"
+  },
+  {
+    "revision": "de2b82e141c7ec4f7483bdf8c1b10321",
+    "url": "/pwa/static/media/sw1.de2b82e1.png"
+  },
+  {
+    "revision": "2a6694cc1dff3d9945417f5dfa62b93a",
+    "url": "/pwa/static/media/sw4serviceWorker.2a6694cc.png"
+  },
+  {
+    "revision": "3978667b5af9deb3cd0fdb8b6d2e0903",
+    "url": "/pwa/static/media/utb.3978667b.png"
+  },
+  {
+    "revision": "91cf4dfcd8874d48ad3640ceb85b3e21",
+    "url": "/pwa/static/media/sw2request.91cf4dfc.png"
+  },
+  {
+    "revision": "ceaa202e57128711ab0181d14cd30ab7",
+    "url": "/pwa/static/media/sw3response.ceaa202e.png"
+  },
+  {
+    "revision": "76c49849db05742a1348417af0473983",
+    "url": "/pwa/static/media/sw5serviceWorkerRequest.76c49849.png"
+  },
+  {
+    "revision": "2f76f21d065d58d619b6dca46ad1f8b9",
+    "url": "/pwa/static/media/sw6serviceWorkerResponse.2f76f21d.png"
+  },
+  {
+    "revision": "999278a692ea35ee66ebbe406d4f901c",
+    "url": "/pwa/static/media/sw7cache.999278a6.png"
+  },
+  {
+    "revision": "ebd1d91c73e1f6f4039a87df7351c4c1",
+    "url": "/pwa/static/media/sw8cacheRequest.ebd1d91c.png"
+  },
+  {
+    "revision": "69a8b182ed2a217713013c0fb7254e01",
+    "url": "/pwa/static/media/sw9cacheLookup.69a8b182.png"
+  },
+  {
+    "revision": "e7193c563a309b88a5dced358233835e",
+    "url": "/pwa/static/media/sw10cacheRetrieve.e7193c56.png"
+  },
+  {
+    "revision": "0ce22b41a75f50769e71ed68a86de3e8",
+    "url": "/pwa/static/media/sw11cacheResponse.0ce22b41.png"
+  },
+  {
+    "revision": "3461c088f7108f51153259f07dc8ed62",
+    "url": "/pwa/static/media/sw12cacheResponseOffline.3461c088.png"
+  },
+  {
+    "revision": "3765cd0fddc2d9c5d9a3",
+    "url": "/pwa/static/js/main.3765cd0f.chunk.js"
+  },
+  {
+    "revision": "22fe8cedafe66da0b77356449395fd35",
+    "url": "/pwa/static/media/newVersionAvailableFullscreen.22fe8ced.png"
+  },
+  {
+    "revision": "ce7cba952c540cc7ea9840fd7432bda7",
+    "url": "/pwa/static/media/updatingServiceWorker.ce7cba95.jpg"
+  },
+  {
+    "revision": "3c6191f390f56facf99037da3a3449c7",
+    "url": "/pwa/static/media/lighthouse-results.3c6191f3.png"
+  },
+  {
+    "revision": "169520126dbcbac64c4a35a7c47268f3",
+    "url": "/pwa/static/media/manifest-tab.16952012.png"
+  },
+  {
+    "revision": "9d782844b68ddc08ea507813037082c3",
+    "url": "/pwa/static/media/serviceworkers-tab.9d782844.png"
+  },
+  {
+    "revision": "1010a6a6f590660725573a14f16fbc12",
+    "url": "/pwa/static/media/storage-tab.1010a6a6.png"
+  },
+  {
+    "revision": "0d88cf9ba03d56ec6e78453b983fe53d",
+    "url": "/pwa/static/media/android-installation.0d88cf9b.gif"
+  },
+  {
+    "revision": "643a02d1c95517d2ad1abadbf30bad02",
+    "url": "/pwa/static/media/ios-installation.643a02d1.gif"
+  },
+  {
+    "revision": "c3d5816b0caa3dea633ef636897dc4e4",
+    "url": "/pwa/static/media/pwa-logo.c3d5816b.png"
+  },
+  {
+    "revision": "1f35b94d08a9c5d49a9542add00a11bc",
+    "url": "/pwa/static/media/app-shell.1f35b94d.jpg"
+  },
+  {
+    "revision": "d501482a93416643a6b8802ef02294ee",
+    "url": "/pwa/static/media/cache-and-network-race.d501482a.png"
+  },
+  {
+    "revision": "f5b2a442c9b909ce3a7c9a74b9b94f1e",
+    "url": "/pwa/static/media/cache-then-network.f5b2a442.png"
+  },
+  {
+    "revision": "5fd0cc2028b0b5962e6f2ee4ac0b3e98",
+    "url": "/pwa/static/media/falling-back-to-network.5fd0cc20.png"
+  },
+  {
+    "revision": "763520b4d4d58cc6aaef14d7e0e3117f",
+    "url": "/pwa/static/media/generic-fallback.763520b4.png"
+  },
+  {
+    "revision": "03cf3c058e1ba5b86ab3aa0543c665a2",
+    "url": "/pwa/static/media/network-falling-back-to-cache.03cf3c05.png"
+  },
+  {
+    "revision": "0598b3e3bc8680ca6a2c",
+    "url": "/pwa/static/css/1.c4732fdc.chunk.css"
+  },
+  {
+    "revision": "bc5cbd3dbb64c2cd060d11393d0d3e08",
+    "url": "/pwa/index.html"
+  }
+];
